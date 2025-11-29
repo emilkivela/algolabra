526ced817261a5fbe89f036526ff17e800e1f649
Kivelä Emil V I <emil.kivela@gmail.com>
2025-11-29 22:47:01 +0200

    Add tests for eigenfaces.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 import os
+import time
 from flask import Flask, render_template, request
 from src.services.helper import convert_pmg
 from src.services.eigenfaces import (
@@ -8,15 +9,24 @@ from src.services.eigenfaces import (
 
 app = Flask(__name__)
 
+start = time.time()
 T_matrix = load_dataset_faces('./static/images/data/')
+faceloadtime = time.time()
 eigenfaces, mean = calculate_eigenfaces(T_matrix)
+eigtime = time.time()
 training_weights, labels = get_training_weights('./static/images/data/', eigenfaces, mean)
-
+weighttime = time.time()
 
 input_files = []
 for file in os.listdir('./static/images/inputs/'):
     base64 = convert_pmg('./static/images/inputs/'+file)
     input_files.append(base64)
+end = time.time()
+
+print("Load dataset faces time:", (faceloadtime-start))
+print("eigface calc time:", (eigtime-start))
+print("Weight time:", (weighttime-start))
+print("Whole preparation time: ", (end-start))
 
 @app.route("/")
 def index():
@@ -30,12 +40,14 @@ def recognise_face():
     """
     Metodi, joka näyttää syötekuvan ja keneksi ohjelma sen tunnisti. Näyttää myös kaikki harjoitusdatan kuvat arvatusta henkilöstä.
     """
+    start = time.time()
+
     if "upload" in request.files:
         input_face_path = request.files["upload"]
     elif "faces" in request.form:
         file = request.form["faces"]
         input_face_path = "./static/images/inputs/"+file
-    
+
     input_image = convert_pmg(input_face_path)
     input_image = input_image["base64"]
 
@@ -47,6 +59,9 @@ def recognise_face():
     for img in os.listdir(path):
         converted = convert_pmg(path+img)
         guess_pictures.append(converted["base64"])
+
+    end = time.time()
+    print("Recognition time: ", (end-start))
     
     return render_template("guess.html", guess=guess[1][1:], distance=guess[0], 
                                          input_image=input_image, true_label=true_label, guesspics= guess_pictures)
---
+++ src/tests/eigenfaces_test.py
@@ -0,0 +1,16 @@
+from src.services.eigenfaces import get_eigs, calculate_eigenfaces
+import numpy as np
+
+def test_get_eigs_finds_correct_values():
+    test_matrix = np.array([[2,0],[0,1]])
+    eigvals, eigvecs = np.linalg.eigh(test_matrix)
+    i = np.argsort(eigvals)[::-1]
+    eigvals_sorted = eigvals[i]
+    eigvecs_sorted = eigvecs[:, i]
+    test_eigvals, test_eigvectors = get_eigs(test_matrix, 2)
+    test_eigvectors = np.column_stack(test_eigvectors)
+    assert np.allclose(test_eigvectors, eigvecs_sorted, atol=1e-02)
+    assert np.allclose(test_eigvals, eigvals_sorted)
+
+
+
